c18c36bfc549a5a7b1d56c640b3a5d060741d825
Albert <aalov@hagerty.com>
2026-05-09 20:59:42 -0300

    feat: implement UIElement wrapper for Selenide-style assertions 🐸✨

--- src/pages/pim-list.page.ts
+++ src/pages/pim-list.page.ts
@@ -1,26 +1,26 @@
-import { Page, Locator } from '@playwright/test';
+import { Page } from '@playwright/test';
 import { BasePage } from '../core/base.page';
 import { TableComponent } from '../components/table.component';
+import { UIElement } from '../core/ui-element';
 
 export class PIMListPage extends BasePage {
   readonly url = '/web/index.php/pim/viewEmployeeList';
   readonly table: TableComponent;
 
-  // Элементы страницы
-  readonly employeeIdInput: Locator;
-  readonly searchBtn: Locator;
-  readonly addBtn: Locator;
+  readonly employeeIdInput: UIElement;
+  readonly searchBtn: UIElement;
+  readonly addBtn: UIElement;
 
   constructor(page: Page) {
     super(page);
     this.table = new TableComponent(page, '.oxd-table');
 
-    // Инициализация локаторов
-    this.employeeIdInput = page
-      .locator('form .oxd-input-group', { hasText: 'Employee Id' })
-      .locator('input');
-    this.searchBtn = page.locator('button[type="submit"]');
-    this.addBtn = page.locator('button:has-text("Add")');
+    this.employeeIdInput = this.element(
+      'form .oxd-input-group:has-text("Employee Id") input',
+      'Employee Id Input'
+    );
+    this.searchBtn = this.element('button[type="submit"]', 'Search Button');
+    this.addBtn = this.element('button:has-text("Add")', 'Add Button');
   }
 
   async searchEmployeeById(id: string): Promise<void> {
